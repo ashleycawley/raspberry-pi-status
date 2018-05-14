102160df6c6095ebaaac795de00634e0884ab3bd
Ashley Cawley <ash@ashleycawley.co.uk>
2018-05-14 19:55:02 +0100

    Added auto page refreshing and commments

--- web-server-code/index.php
+++ web-server-code/index.php
@@ -1,5 +1,12 @@
 <?php
-  for ( $i = 0; $i < 8; $i++ ) {
-    echo "<iframe src='./pies/pi$i.html' height='525' width='380' frameBorder='1'></iframe>";
+
+# Refreshes the page after a certain duration
+$url1=$_SERVER['REQUEST_URI'];
+header("Refresh: 300; URL=$url1");
+
+# Increments a integer varaible which is later used to call upon a webpage in an iFrame
+  for ( $i = 1; $i < 8; $i++ ) {
+    echo "<iframe src='./pies/pi$i.html' height='450' width='380' frameBorder='1'></iframe>";
 }
+
 ?>
